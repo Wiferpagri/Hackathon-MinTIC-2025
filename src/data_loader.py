2c551ebac6076cd9960bf0939a94f653ed6b0799
William Pabon <wiferpagri19@gmail.com>
2025-11-30 21:10:41 -0500

    feat: agrega notebook de modelo de detección de anomalías

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,26 +1,25 @@
-import polars as pl
+import pandas as pd
 
 from utils.conectar_databricks import ejecutar_query_databricks
 
 
-def obtener_datos_polars(query: str):
-    """Obtiene datos desde Databricks y los carga en un DataFrame de Polars.
+def obtener_datos_pandas(query: str):
+    """Obtiene datos desde Databricks y los carga en un DataFrame de Pandas.
 
     Args:
         query: Consulta SQL para ejecutar.
 
     Returns:
-        DataFrame de Polars con los datos obtenidos.
-
+        DataFrame de Pandas con los datos obtenidos.
     """
 
     filas, descripcion_cursor = ejecutar_query_databricks(query)
 
     if not filas:
-        return pl.DataFrame()
+        return pd.DataFrame()
 
     columnas = [desc[0] for desc in descripcion_cursor]
 
-    df = pl.DataFrame(data=filas, schema=columnas)
+    df = pd.DataFrame(data=filas, columns=columnas)
 
     return df
